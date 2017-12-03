abb9041ac424b45e31c028eea99531e7b287f8dc
Arjun Bhandage <arjun.bhandage@nokia.com>
2017-12-03 16:12:18 +0530

    commit

--- app.py
+++ app.py
@@ -1,19 +1,64 @@
 import cherrypy
 import os
+import json
+import requests
+import redis
+import ast
+from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('html'))
+urlGainers = "https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json"
+urlLosers = "https://www.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json"
 
-class HelloWorld(object):
+
+class GainersAndLosers(object):
+
     @cherrypy.expose
     def index(self):
-        data_to_show = ['Hello', 'world']
+        r = redis.Redis(host="ec2-34-233-217-71.compute-1.amazonaws.com", port="43969", db=0, password="p7551f57171f509a98262374f76c4aa523dd3640f6db8312dbcbf0a886791397d")
+      
+        resp = requests.get(urlGainers)
+        json_data_gainers = json.loads(resp.text)
+        #resp = requests.get(urlLosers)
+        #json_data_losers = json.loads(resp.text)
+        r.hmset("topGainers", json_data_gainers)
+        #r.hmset("topLosers", json_data_losers)
+        print "Redis updated"
+        #topLosers = r.hgetall("topLosers")
+        topGainers = r.hgetall("topGainers")
+        markup = ""
+        topGainers = topGainers['data'].replace("u'","\"")
+        topGainers = topGainers.replace("'","\"")
+        jsonTopGainers = json.loads(topGainers)
+        for data in jsonTopGainers:
+            #percentageChange = ((float(data["ltp"])-float(data["previousPrice"]))/float(data["previousPrice"]))*100
+            markup += """ <div class="card">
+            <img src="./assets/img_avatar.png" alt="Avatar" style="width:10%">
+            <div class="container">
+                <h4><b>Symbol: """+data["symbol"]+"""</b></h4>
+                <p>LTP: """+data["ltp"]+"""</p> 
+                <p>Open: """+data["openPrice"]+"""</p> 
+                <p>High: """+data["highPrice"]+"""</p> 
+                <p>Low: """+data["lowPrice"]+"""</b></p> 
+                <p>Previous Price: """+data["previousPrice"]+"""</p> 
+                <p>Net Price: """+data["netPrice"]+"""</p> 
+                <p>Traded Quantity: """+data["tradedQuantity"]+"""</p>
+                <p>Turnover: """+data["turnoverInLakhs"]+""" Lakhs</p> 
+                <p>"""+data["lastCorpAnnouncementDate"]+"""</p> 
+                <p>"""+data["lastCorpAnnouncement"]+"""</p>
+            </div>
+            </div>"""
+        data_to_show = [markup]
         tmpl = env.get_template('index.html')
         return tmpl.render(data=data_to_show)
 
+  
 config = {
     'global': {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 5000)),
+		#'server.socket_host': '127.0.0.1',
+        #'server.socket_port': 8080,
     },
     '/assets': {
         'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
@@ -22,4 +67,4 @@ config = {
     }
 }
 
-cherrypy.quickstart(HelloWorld(), '/', config=config)
+cherrypy.quickstart(GainersAndLosers(), '/', config=config)
